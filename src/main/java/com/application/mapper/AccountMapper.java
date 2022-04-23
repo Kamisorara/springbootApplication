ac29dbc25d0c57535451848db2a08edb9150d612
kamisora <1210281722@qq.com>
2022-04-23 22:03:46 +0800

    version2.1

--- src/main/java/com/application/mapper/AccountMapper.java
+++ src/main/java/com/application/mapper/AccountMapper.java
@@ -2,7 +2,9 @@ package com.application.mapper;
 
 import com.application.entity.Account;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface AccountMapper extends BaseMapper<Account> {
 
 }
--- src/main/java/com/application/mapper/UserMapper.java
+++ src/main/java/com/application/mapper/UserMapper.java
@@ -2,6 +2,9 @@ package com.application.mapper;
 
 import com.application.entity.Users;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper extends BaseMapper<Users> {
+
 }
